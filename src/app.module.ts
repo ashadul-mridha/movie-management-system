f28f13123c3afd5c79c9811922c7f742fb6b3f71
Ashadul Islam <ashadulmridhaprog@gmail.com>
2025-01-24 21:02:17 +0600

    init: user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from './config/app/config.module';
+import { MysqlDatabaseProviderModule } from './providers/database/mysql/provider.module';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [AppConfigModule, MysqlDatabaseProviderModule],
   controllers: [AppController],
   providers: [AppService],
 })
