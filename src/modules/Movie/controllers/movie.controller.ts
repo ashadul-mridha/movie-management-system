67421fdc1989b6d590746b0aaf28d40874606a55
Ashadul Islam <ashadulmridhaprog@gmail.com>
2025-01-25 11:51:06 +0600

    feat: get all movies

--- src/modules/Movie/controllers/movie.controller.ts
+++ src/modules/Movie/controllers/movie.controller.ts
@@ -1,15 +1,18 @@
 import {
   Body,
   Controller,
+  Get,
   Param,
   ParseIntPipe,
   Patch,
   Post,
+  Query,
   UseGuards,
   Version,
 } from '@nestjs/common';
 import { UserTypes } from '../../../common/decorators/user-type.decorator';
 import { GetUser } from '../../../common/decorators/user.decorator';
+import { QueryDto } from '../../../common/dtos/query.dto';
 import { UserRequest } from '../../../common/dtos/user-req.dto';
 import { UserType } from '../../../common/enums/user.enums';
 import { throwError } from '../../../common/errors/errors.function';
@@ -51,6 +54,52 @@ export class MovieController {
   ) {
     try {
       return await this.movieService.update(userInfo, id, updateMovieDto);
+    } catch (error) {
+      throwError(error.status, [], error.message);
+    }
+  }
+
+  // get all movies
+  @Get()
+  @Version('1')
+  @UserTypes(UserType.USER)
+  @UseGuards(JwtAuthGuard, UserTypeGuard)
+  async allMovies(@GetUser() userInfo: UserRequest, @Query() query: QueryDto) {
+    try {
+      return await this.movieService.allMovies(
+        userInfo,
+        query.perPage && +query.perPage,
+        query.currentPage && query.currentPage - 1,
+      );
+    } catch (error) {
+      throwError(error.status, [], error.message);
+    }
+  }
+
+  // view user own movies
+  @Get('my-movies')
+  @Version('1')
+  @UserTypes(UserType.USER)
+  @UseGuards(JwtAuthGuard, UserTypeGuard)
+  async myMovies(@GetUser() userInfo: UserRequest) {
+    try {
+      return await this.movieService.myMovies(userInfo);
+    } catch (error) {
+      throwError(error.status, [], error.message);
+    }
+  }
+
+  // get a movie by id
+  @Get(':id')
+  @Version('1')
+  @UserTypes(UserType.USER)
+  @UseGuards(JwtAuthGuard, UserTypeGuard)
+  async getMovieById(
+    @GetUser() userInfo: UserRequest,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    try {
+      return await this.movieService.getMovieById(userInfo, id);
     } catch (error) {
       throwError(error.status, [], error.message);
     }
--- src/modules/Movie/services/movie.service.ts
+++ src/modules/Movie/services/movie.service.ts
@@ -14,6 +14,21 @@ export class MovieService {
     private movieRepository: Repository<Movie>,
   ) {}
 
+  select = {
+    id: true,
+    title: true,
+    description: true,
+    releasedAt: true,
+    duration: true,
+    genre: true,
+    language: true,
+    avgRating: true,
+    totalRatings: true,
+    createdBy: true,
+    createdAt: true,
+    updatedAt: true,
+  };
+
   // create a new movie
   async create(userInfo: UserRequest, createMovieDto: CreateMovieDto) {
     console.log(userInfo);
@@ -59,5 +74,71 @@ export class MovieService {
     }
 
     return await this.movieRepository.findOne({ where: { id: id } });
+  }
+
+  // view all movies
+  async allMovies(
+    userInfo: UserRequest,
+    perPage: number = 10,
+    currentPage: number = 0,
+  ) {
+    // get all movies
+    const [movies, total] = await this.movieRepository.findAndCount({
+      where: { createdBy: userInfo.id },
+      select: this.select,
+      take: perPage,
+      skip: currentPage * perPage,
+    });
+
+    // if no movie found
+    if (!movies.length) {
+      throwError(HttpStatus.NOT_FOUND, [], 'No movie found');
+    }
+
+    return {
+      data: movies,
+      perPage: perPage,
+      currentPage: currentPage + 1,
+      totalPage: Math.ceil(total / perPage),
+      totalResult: total,
+    };
+  }
+
+  // view user own movies
+  async myMovies(userInfo: UserRequest) {
+    const myMovies = await this.movieRepository.find({
+      where: { createdBy: userInfo.id },
+      select: this.select,
+    });
+
+    // if no movie found
+    if (!myMovies.length) {
+      throwError(HttpStatus.NOT_FOUND, [], 'No movie found');
+    }
+
+    return {
+      data: myMovies,
+      message: 'Movies found',
+      status: HttpStatus.OK,
+    };
+  }
+
+  // get a movie by id
+  async getMovieById(userInfo: UserRequest, id: number) {
+    const movie = await this.movieRepository.findOne({
+      where: { id: id },
+      select: this.select,
+    });
+
+    // if no movie found
+    if (!movie) {
+      throwError(HttpStatus.NOT_FOUND, [], 'Movie not found');
+    }
+
+    return {
+      data: movie,
+      message: 'Movie found',
+      status: HttpStatus.OK,
+    };
   }
 }
